6d513c8d0d7017fd3974db5649b58009e3b09669
albert203 <albert.jordaan@outlook.com>
2024-03-23 14:32:23 +1300

    way to write clean code for html using php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello World!";
+    ?>
+
+    <!-- good syntax for incorperating html elements into php code -->
+    <?php if (true) { ?>
+        <h1>True</h1>
+    <?php } ?>
+</body>
+</html>
